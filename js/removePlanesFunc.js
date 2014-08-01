7f2f52b2e53cc6515908522813de0f9a89baef09
TzvetanIG <cecoig@mail.bg>
2014-08-01 14:21:19 +0300

    clean trash

--- js/removePlanesFunc.js
+++
@@ -1,6 +0,0 @@
-function removePlanes(){
-    var allPlanes=document.getElementsByName('plane');
-    for(var i=0; i<allPlanes.length; i++){
-        allPlanes[i].parentNode.removeChild(allPlanes[i]);
-    }
-}
